c9c109323e97d7a7c5d32f75aca3521a0452ab19
Kennedy Musau <kennedy.musauu@gmail.com>
2023-05-13 08:22:01 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
 
 function App() {
   return (
@@ -6,8 +9,10 @@ function App() {
       <NavBar />
       <Home />
       <About />
+
     </div>
   );
 }
 
 export default App;
+
